f133d902bfa81cabb9b8e50073e7683d9bf9a949
Rodrigo Lapenta <rodrigoguille@gmail.com>
2017-09-28 00:57:19 -0300

    21

--- app.js
+++ app.js
@@ -60,11 +60,10 @@ bot.on('message', msg => {
 					{$inc: {visit_count: 1}},
 					{upsert: true, safe: false},
 					function(err, res) {
-						console.log(res.nModified);
+						console.log("res ----------> ", JSON.stringify(res));
 						if (err) throw err;
-						if(res.nModified == 1) bot.sendMessage(msg.chat.id, "¡Bienvenido de nuevo, " + msg.chat.first_name + " " + msg.chat.last_name + "!");
+						if(JSON.parse(res).nModified == 1) bot.sendMessage(msg.chat.id, "¡Bienvenido de nuevo, " + msg.chat.first_name + " " + msg.chat.last_name + "!");
 						else bot.sendMessage(msg.chat.id, "¡Bienvenido a mi bot, " + msg.chat.first_name + " " + msg.chat.last_name + "!");
-						console.log("1 document inserted");
 						db.close();
 					}
 				);
