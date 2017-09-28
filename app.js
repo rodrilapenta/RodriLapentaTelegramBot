20ffdd5455d19235eaef8bc293222035033d46c0
Rodrigo Lapenta <rodrigoguille@gmail.com>
2017-09-27 23:42:34 -0300

    12
    test

--- app.js
+++ app.js
@@ -50,39 +50,35 @@ app.listen(port, () => {
 
 // Just to ping!
 bot.on('message', msg => {
-	//if(msg) {
-		bot.sendMessage(msg.chat.id, msg);
-	/*}
-	else {
-		switch(msg.text) {
-			case "/start":
-				bot.sendMessage(msg.chat.id, "¡Bienvenido a mi bot, " + msg.chat.first_name + " " + msg.chat.last_name + "!");
-				MongoClient.connect(mongoUrl, function(err, db) {
+	
+	switch(msg.text) {
+		case "/start":
+			bot.sendMessage(msg.chat.id, "¡Bienvenido a mi bot, " + msg.chat.first_name + " " + msg.chat.last_name + "!");
+			/*MongoClient.connect(mongoUrl, function(err, db) {
+				if (err) throw err;
+				db.collection("bot_users").insertOne(req.body, function(err, res) {
 					if (err) throw err;
-					db.collection("bot_users").insertOne(req.body, function(err, res) {
-						if (err) throw err;
-						console.log("1 document inserted");
-						db.close();
-					});
+					console.log("1 document inserted");
+					db.close();
 				});
-			break;
-			case "/saludar":
-				bot.sendMessage(msg.chat.id, "¡Hola " + msg.chat.first_name + " " + msg.chat.last_name + "!");
-			break;
-			case "/debug":
-				bot.sendMessage(msg.chat.id, msg);
-			break;
-			case "/consumos":
-				request(process.env.SM_CONS_URL, function (error, response, body) {
-					if (!error && response.statusCode == 200) {
-						var consumos = JSON.parse(body);
-						bot.sendMessage(msg.chat.id, "Usted tiene " + consumos.length + " consumos en el mes de septiembre.");
-					}
-				})
-			break;
-			default:
-				bot.sendMessage(msg.chat.id, "No reconocimos tu comando.");
-			break;
-		}
-	}*/
+			});*/
+		break;
+		case "/saludar":
+			bot.sendMessage(msg.chat.id, "¡Hola " + msg.chat.first_name + " " + msg.chat.last_name + "!");
+		break;
+		case "/testing":
+			bot.sendMessage(msg.chat.id, msg);
+		break;
+		case "/consumos":
+			request(process.env.SM_CONS_URL, function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var consumos = JSON.parse(body);
+					bot.sendMessage(msg.chat.id, "Usted tiene " + consumos.length + " consumos en el mes de septiembre.");
+				}
+			})
+		break;
+		default:
+			bot.sendMessage(msg.chat.id, "No reconocimos tu comando.");
+		break;
+	}
 });
